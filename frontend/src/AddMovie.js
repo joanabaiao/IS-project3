d38cf8f82321be4d6baaf7a9b91f4e5455e7b685
joanabaiao <joanabaiao99@gmail.com>
2021-08-11 18:19:55 +0100

    code

---
+++ frontend/src/AddMovie.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import FormMovie from './FormMovie'
+
+const AddMovie = () => {
+
+  return (
+    <div className="add_movie">
+      <FormMovie />
+    </div>
+    
+  );
+}
+ 
+export default AddMovie;
+
+
---
+++ frontend/src/EditMovie.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import FormEdit from './FormEdit.js'
+
+const EditMovie = () => {
+
+  return (
+    <div className="edit_movie">
+      <FormEdit />
+    </div>
+    
+  );
+}
+ 
+export default EditMovie;
